aacd1a71317999229c06ff81dd8cb0630277a474
Dazz <iamzagoruiko@gmail.com>
2021-03-18 15:32:36 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,85 @@
+"use strict";
+
+let incr = 10,
+    decr = 10;
+// incr++;
+// decr--;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(incr);
+console.log(decr);
+
+
+
+
+
+
+
+
+
+
+// console.log('arr' + "- object");
+
+
+
+
+
+
+
+
+
+
+
+// const category = 'guns';
+
+// console.log(`https://someurl.com/${category}`);
+
+// const user = "Ivan";
+
+// alert(`Hello, ${user}`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const obj = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+// };
+
+// console.log(obj ["name"]);
+
+// let arr = [`plum.png`,`orange.jpg`,`apple.bmp`];
+// console.log(arr[1]);
+
+// const result = confirm(`Are you here?`);
+// console.log(result);
+
+// const answer = +prompt("Вам есть 18","18");
+// console.log(answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt('What is your name?', '');
+// answers[1] = prompt('What is your surname?', '');
+// answers[3] = prompt('How old are you?', '');
+
+// console.log(typeof(answers));
+// console.log(typeof(null));
+
